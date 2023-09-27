35ba21b254a99ae061fa5027f84129b60f0b9b92
JuanTomasL <juantomaslacave@gmail.com>
2023-09-26 21:03:54 -0300

    Galeria imagenes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Navbar from './components/Navbar.jsx'
-
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Navbar from './components/Navbar.jsx';
+import ImgGallery from './components/ImgGallery.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
-  </React.StrictMode>,
-)
+    <ImgGallery />
+  </React.StrictMode>
+
+
+
+);
+
+
+
+
