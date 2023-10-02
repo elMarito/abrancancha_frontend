0e91ab7acf5408ed6534c988d37b16795b9468c8
Nicolas-Mansilla <nicolasmasnsilla6284@gmail.com>
2023-10-02 10:56:57 -0300

    Cree el banner con sus estilos , espero que haya quedado mas o menos bien

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Navbar from './components/Navbar.jsx'
+import Banner from './components/Banner.jsx'
+
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar />
+    <Banner/>
   </React.StrictMode>,
 )
