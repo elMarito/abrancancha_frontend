f2f636d0bbf39259f340b0ca69caffa361501e6f
Nicolas-Mansilla <nicolasmasnsilla6284@gmail.com>
2023-10-02 17:51:59 -0300

    Aca se agrego el banner al main Nicolas mansilla

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <App />
       </BrowserRouter> */}
       <Navbar />
+      <Banner/>
     {/* </AppContextProvider> */}
   </React.StrictMode>,
 )
