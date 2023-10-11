73711dcb544682ed5e7c7b787dfa964dd8d6baeb
abanchi <agustin.banchi@uosolutions.com>
2023-10-11 11:01:12 -0300

    actualizacion de rama main, se incorpora banner, galeria y footer al home, se actualizan los componentes en carpeta para un mejor manejo del flujo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../Banner/Banner'
 import Footer from '../Footer/Footer'
+import ImgGallery from '../Gallery/ImgGallery'
 function Home() {
   return (
     <>
         <Banner></Banner>
+        <ImgGallery></ImgGallery>
         <Footer></Footer>
     </>
   )
