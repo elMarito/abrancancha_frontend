d2a2372d5e8c18974f4157363b60817888f13539
abanchi <agustin.banchi@uosolutions.com>
2023-11-13 13:09:42 -0300

    Cambios en Navbarcack, se quitaron las url que no se usaban

--- src/components/HomeBack/NavbarBack.jsx
+++ src/components/HomeBack/NavbarBack.jsx
@@ -10,8 +10,14 @@ function NavBarBack() {
     <>
       <nav className="navbar navbar-expand-lg  static-top">
         <div className="container">
-          <ul className="navbar-nav">
+         
           <Link to='/' className="navbar-brand"><img src="src/assets/abc_logo.svg" alt="" width="90" /></Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+            <ul className="navbar-nav">
+            
             <Link to="/cronograma" className="nav-link" aria-current="page">
               <button className="btn btn-outline-success">Cronograma</button>
             </Link>
@@ -25,9 +31,7 @@ function NavBarBack() {
             <Link to="/usuario" className="nav-link" aria-current="page">
               <button className="btn btn-outline-success me-2">Usuario</button>
             </Link>
-            <Link to="/Administradores" className="nav-link" aria-current="page">
-              <button className="btn btn-outline-success me-2">Administradores</button>
-            </Link>
+            
             <Link to="/configuracion" className="nav-link" aria-current="page">
               <button className="btn btn-outline-success">Configuración</button>
             </Link>
@@ -35,6 +39,7 @@ function NavBarBack() {
             <button onClick={Outsesion} className="btn btn-outline-success">Cerrar sesión</button>
             
           </ul>
+        </div>
         </div>
       </nav>
     </>
